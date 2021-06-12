9c3b5e9715ca739f9990fc6f63522221abb74e52
Andrew <Andrew@DESKTOP-AEVHUV9>
2021-06-12 13:55:48 -0400

    Add oxygen, replenish and warning audio

---
+++ Assets/Oxygen.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oxygen : MonoBehaviour {
+    public int value = 100;
+    public AudioSource warning;
+
+    bool isWarningCheckValid = true;
+
+    void Start() {
+        StartCoroutine(Deplete());
+    }
+
+    private void Update() {
+        CheckOxygenThreshold();
+    }
+
+    public void Replenish() {
+        isWarningCheckValid = true;
+        value = 100;
+    }
+
+    IEnumerator Deplete() {
+        while (value > 0) {
+            value-= 5;
+            yield return new WaitForSeconds(0.5f);
+        }
+        Debug.LogWarning("Out of oxygen.");
+    }
+
+    void CheckOxygenThreshold() {
+        if (value <= 50 && isWarningCheckValid) {
+            Debug.Log("Warn");
+            warning.Play();
+            isWarningCheckValid = false;
+        }
+    }
+}
