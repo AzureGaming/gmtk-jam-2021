ff0bf8583d27bc6ed4618896fe44c3eb1b11669a
Andrew <Andrew@DESKTOP-AEVHUV9>
2021-06-12 11:19:29 -0400

    Add functioning hook and rope

--- Assets/RopeScript.cs
+++ Assets/RopeScript.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class RopeScript : MonoBehaviour {
     public Vector2 destination;
     public float speed = 1f;
-     float distance = 2f;
     public GameObject nodePrefab;
 
+    float distanceBetweenNodes = 0.5f;
     GameObject player;
     GameObject lastNode;
     bool isDone = false;
+    List<GameObject> nodes = new List<GameObject>();
+    int vertexCount = 2; // hook and player
+    LineRenderer lr;
+
+    private void Awake() {
+        lr = GetComponent<LineRenderer>();
+    }
 
     private void Start() {
         player = GameObject.Find("Player");
         lastNode = gameObject;
+
+        nodes.Add(lastNode);
     }
 
     private void Update() {
@@ -22,22 +31,25 @@ public class RopeScript : MonoBehaviour {
 
         if ((Vector2)transform.position != destination) {
             // Hook is travelling
-            if (Vector2.Distance(player.transform.position, lastNode.transform.position) > distance) {
+            if (Vector2.Distance(player.transform.position, lastNode.transform.position) > distanceBetweenNodes) {
                 CreateNode();
             }
         } else if (!isDone) {
             isDone = true;
-            Debug.Log("done");
+            // if hook gets to position before all nodes are created, create the rest
+            while (Vector2.Distance(player.transform.position, lastNode.transform.position) > distanceBetweenNodes) {
+                CreateNode();
+            }
             lastNode.GetComponent<HingeJoint2D>().connectedBody = player.GetComponent<Rigidbody2D>();
         }
 
-
+        RenderLine();
     }
 
     void CreateNode() {
         Vector2 pos = player.transform.position - lastNode.transform.position;
         pos.Normalize();
-        pos *= distance;
+        pos *= distanceBetweenNodes;
         pos += (Vector2)lastNode.transform.position;
 
         GameObject node = Instantiate(nodePrefab, pos, Quaternion.identity);
@@ -46,5 +58,17 @@ public class RopeScript : MonoBehaviour {
 
         lastNode.GetComponent<HingeJoint2D>().connectedBody = node.GetComponent<Rigidbody2D>();
         lastNode = node;
+
+        nodes.Add(lastNode);
+        vertexCount++; // account for new node
+    }
+
+    void RenderLine() {
+        lr.positionCount = vertexCount;
+        for (int i = 0; i < nodes.Count; i++) {
+            lr.SetPosition(i, nodes[i].transform.position);
+        }
+        //account for player
+        lr.SetPosition(nodes.Count, player.transform.position);
     }
 }
--- Assets/Scripts/ThrowHook.cs
+++ Assets/Scripts/ThrowHook.cs
@@ -2,18 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThrowHook : MonoBehaviour
-{
+public class ThrowHook : MonoBehaviour {
     public GameObject hookPrefab;
 
     GameObject currentHook;
+    bool isHookActive = false;
 
     private void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            Vector2 destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            currentHook = Instantiate(hookPrefab, transform.position, Quaternion.identity);
-
-            currentHook.GetComponent<RopeScript>().destination = destination;
+            if (isHookActive) {
+                Destroy(currentHook);
+                CreateHook();
+            } else {
+                CreateHook();
+                isHookActive = true;
+            }
         }
+    }
+
+    void CreateHook() {
+        Vector2 destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        currentHook = Instantiate(hookPrefab, transform.position, Quaternion.identity);
+
+        currentHook.GetComponent<RopeScript>().destination = destination;
+
     }
 }
