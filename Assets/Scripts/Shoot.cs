6ea84bfa3bf56b9e885f1c4e3e8db46b1582e039
Andrew <Andrew@DESKTOP-AEVHUV9>
2021-06-11 23:33:24 -0400

    Add animated ropes

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class Shoot : MonoBehaviour {
     public Transform origin;
     public GameObject projectilePrefab;
+    public GameObject lineRope;
 
     private void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetMouseButtonDown(0)) {
             SpawnProjectile();
         }
     }
 
     void SpawnProjectile() {
-        GameObject projectile = Instantiate(projectilePrefab, origin);
-        //projectile.GetComponent<HingeJoint2D>()
+        GameObject projectile = Instantiate(projectilePrefab, origin.position, Quaternion.identity);
+        projectile.GetComponent<Rigidbody2D>().AddForce(origin.right * 5, ForceMode2D.Impulse);
+        GameObject line = Instantiate(lineRope);
+        line.GetComponent<LineRope>().destination = projectile.transform;
     }
 }
