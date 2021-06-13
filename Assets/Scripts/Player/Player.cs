ccc2cea923402ed71f1c0255a4ba003e3a350261
Andrew <Andrew@DESKTOP-AEVHUV9>
2021-06-13 10:08:41 -0400

    Add choking

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : PlayerState {
-    public delegate void Grapple();
-    public static Grapple OnGrapple;
+    public delegate void OxygenDepleted();
+    public static OxygenDepleted OnOxygenDepleted;
 
     public GameObject connectedAbove;
     public GameObject connectedBelow;
     public PlayerState state;
 
+    public AudioSource breathing;
+    public AudioSource choking;
+
+    private void OnEnable() {
+        OnOxygenDepleted += DeathFromOxygen;
+    }
+
+    private void OnDisable() {
+        OnOxygenDepleted -= DeathFromOxygen;
+    }
+
     void Update() {
         state = state.HandleInput();
+    }
+
+    private void Start() {
+        breathing.Play();
+    }
+
+    void DeathFromOxygen() {
+        breathing.Stop();
+        choking.Play();
     }
 }
